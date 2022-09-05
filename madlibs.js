c254532b8fa4dd819d5e48a8242eeb8f5ce468ed
CamJamieson <camjamesj@gmail.com>
2022-09-04 20:41:18 -0400

    Added Word Variables

--- madlibs.js
+++ madlibs.js
@@ -1,9 +1,9 @@
-let noun = [];
-let adjective = [];
-let verb = [];
-let adverb = [];
-let pluralNoun = [];
-let partOfTheBody = [];
+let noun = ["animal", "beast", "beggar", "monster", "criminal" , "skipper", "crook"];
+let adjective = ["beautiful", "majestic", "fancy", "extravagant", "powerful"];
+let verb = ["yell", "shout", "bellow", "call out"];
+let adverb = ["fully", "rightfully", "totally", "completely"];
+let pluralNoun = ["matey", "chaps", "people", "strangers"];
+let partOfTheBody = ["arm", "limb", "leg", "hand", "finger"];
 
 let myMadLibs = `Ye can always pretend to be a bloodthirsty (noun), threatening everyone by waving yer (adjective) sword in the air, but until ye learn to (verb) like a pirate, ye'll never be (adverb) accepted as an authentic (noun). So here's what ye do: Cleverly work into yer daily converations (adjective) pirate phrases such as "Ahoy there, (plural-noun)", "Avast, ye (plural-noun)", and "Shiver me (plural-noun)". Remember to drop all yer gs when ye say such words as sailin', splittin', and fightin'. This will give ye a/an (part-of-the-body) start to being recognized as a swashbucklin' (noun). Once ye have the lingo down pat, it helps to wear a three-cornered (noun) on yer head, stash a/an (noun) in yer pants, and keep a/an (noun) perched atop yer (part-of-the-body). Aye, now ye be a real pirate!`;
 
